67c0b3dc8693547e0a262d41f27970cfee9c2480
Jose Ignacio <joserobleda@gmail.com>
2014-11-26 21:42:51 +0100

    first version

---
+++ app.js
@@ -0,0 +1,43 @@
+(function(){
+	function l(s) {
+		if (console) {
+			console.log(s);
+		}
+	};
+
+	function getJSON(URL, cb) {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				cb(JSON.parse(xhr.responseText));
+			}
+		};
+		xhr.open("GET", URL, false);
+		xhr.setRequestHeader('Accepts', 'application/json');
+		xhr.send(null);
+	}
+
+	function run () {
+		var nodes, usernames, collection = {}, URI, pieces, repo;
+
+		pieces 	= location.pathname.split('/');
+		repo 	= pieces[1] + '/' + pieces[2];
+		URI 	= 'http://gitscoring.herokuapp.com/' + repo;
+
+		l('repo endpoint: ' + URI);
+
+
+		nodes = document.querySelectorAll(".timeline-comment-wrapper.js-comment-container > a");
+
+		if (nodes.length === 0) {
+			return true;
+		}
+
+		getJSON(URI, function (res) {
+			log('gs data recieved', res);
+		});
+	}
+
+	l('git score extension loaded');
+	window.addEventListener('load', run, false);
+}());
